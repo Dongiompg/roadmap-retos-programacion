ccace135e53ec5033c2c4f973a7cce9183f733c0
Dongiompg <giovannimanuelpadrongandino@gmail.com>
2025-05-17 13:25:33 +0200

    Create Dongiompg.py

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/Dongiompg.py
@@ -0,0 +1,35 @@
+#https://www.python.org/
+
+#comentario en una sola linea
+
+"""
+comentarios
+de
+varias
+lineas
+"""
+my_variable = "variable 1"
+my_variable = "variable 2"
+
+#en pyton no se puede hacer constantes...
+
+MY_CONSTANT = "INTENTO DE CONSTANTE"
+MY_CONSTANT = "dONGIO"
+
+#lenguajes primitivos pyton
+
+"""
+Enteros
+flotentes
+booleanos verdadero o falso
+strings texto
+"""
+
+
+my_int = 31
+my_float  = 2.183
+my_bool = true
+My_bool = false
+my_strigs = "Texto en cadena se puede poner en comillas simples o dobles"
+
+print(" hola mundon pyphon ")
